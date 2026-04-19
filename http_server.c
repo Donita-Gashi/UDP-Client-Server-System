d69cf99e8dc11c5c03d673c11a6d0014d0515491
Donita-Gashi <161389888+Donita-Gashi@users.noreply.github.com>
2026-04-19 19:22:38 +0200

    Update http_server.c

--- http_server.c
+++ http_server.c
@@ -8,51 +8,55 @@
 
 #define PORT 8080
 
-void *http_server(void *arg) {
-    int server_fd, new_socket;
-    struct sockaddr_in address;
-    int addrlen = sizeof(address);
+void *handle_request(void *arg) {
+    int new_socket = *(int*)arg;
+    free(arg);
+
     char buffer[3000];
+    memset(buffer, 0, sizeof(buffer));
+    read(new_socket, buffer, sizeof(buffer));
 
-    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (strstr(buffer, "GET /stats") != NULL) {
 
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(PORT);
+        char clients_info[1000] = "";
+        char logs_info[2000] = "";
+        char body[4000];
+        char response[5000];
 
-    bind(server_fd, (struct sockaddr *)&address, sizeof(address));
-    listen(server_fd, 5);
+        // klientet
+        for (int i = 0; i < client_count; i++) {
+            char temp[100];
+            sprintf(temp, "Client %d: %s\n", i + 1, clients[i].ip);
+            strcat(clients_info, temp);
+        }
 
-    printf("HTTP server running on port %d...\n", PORT);
+        // log-et
+        for (int i = 0; i < total_messages && i < MAX_LOGS; i++) {
+            strcat(logs_info, logs[i]);
+            strcat(logs_info, "\n");
+        }
 
-    while (1) {
-        new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen);
+        int is_json = strstr(buffer, "format=json") != NULL;
 
-        memset(buffer, 0, sizeof(buffer));
-        read(new_socket, buffer, 3000);
-
-        if (strstr(buffer, "GET /stats") != NULL) {
-
-            char response[5000];
-            char clients_info[1000] = "";
-            char logs_info[2000] = "";
-
-            // klientët
-            for (int i = 0; i < client_count; i++) {
-                char temp[100];
-                sprintf(temp, "Client %d: %s\n", i+1, clients[i].ip);
-                strcat(clients_info, temp);
-            }
-
-            // log-et
-            for (int i = 0; i < total_messages && i < MAX_LOGS; i++) {
-                strcat(logs_info, logs[i]);
-                strcat(logs_info, "\n");
-            }
+        if (is_json) {
+            sprintf(body,
+                "{\n"
+                "  \"active_clients\": %d,\n"
+                "  \"total_messages\": %d\n"
+                "}\n",
+                client_count, total_messages
+            );
 
             sprintf(response,
                 "HTTP/1.1 200 OK\r\n"
-                "Content-Type: text/plain\r\n\r\n"
+                "Content-Type: application/json\r\n"
+                "Content-Length: %ld\r\n\r\n"
+                "%s",
+                strlen(body), body
+            );
+
+        } else {
+            sprintf(body,
                 "Active clients: %d\n"
                 "Total messages: %d\n\n"
                 "Clients:\n%s\n"
@@ -60,9 +64,71 @@ void *http_server(void *arg) {
                 client_count, total_messages, clients_info, logs_info
             );
 
-            send(new_socket, response, strlen(response), 0);
+            sprintf(response,
+                "HTTP/1.1 200 OK\r\n"
+                "Content-Type: text/plain\r\n"
+                "Content-Length: %ld\r\n\r\n"
+                "%s",
+                strlen(body), body
+            );
         }
 
-        close(new_socket);
+        send(new_socket, response, strlen(response), 0);
+    } else {
+        char *not_found =
+            "HTTP/1.1 404 Not Found\r\n"
+            "Content-Type: text/plain\r\n\r\n"
+            "Not Found";
+
+        send(new_socket, not_found, strlen(not_found), 0);
     }
+
+    close(new_socket);
+    return NULL;
+}
+
+
+void *http_server(void *arg) {
+    int server_fd;
+    struct sockaddr_in address;
+    int addrlen = sizeof(address);
+
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_fd < 0) {
+        perror("socket failed");
+        exit(EXIT_FAILURE);
+    }
+
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(PORT);
+
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
+        perror("bind failed");
+        exit(EXIT_FAILURE);
+    }
+
+    if (listen(server_fd, 10) < 0) {
+        perror("listen failed");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("HTTP server running on port %d...\n", PORT);
+
+    while (1) {
+        int *new_socket = malloc(sizeof(int));
+        *new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen);
+
+        if (*new_socket < 0) {
+            perror("accept failed");
+            free(new_socket);
+            continue;
+        }
+
+        pthread_t tid;
+        pthread_create(&tid, NULL, handle_request, new_socket);
+        pthread_detach(tid);
+    }
+
+    return NULL;
 }
