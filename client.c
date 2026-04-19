e369c8266ec6df4d1427f327cdc5b43f86b7fe61
zana-ugraj <zana.ugraj@student.uni-pr.edu>
2026-04-19 20:27:10 +0200

    Update client.c

--- client.c
+++ client.c
@@ -1,1 +1,81 @@
+#include <string.h>
+#include <stdlib.h>
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <stdio.h>
 
+#pragma comment(lib, "ws2_32.lib")
+
+#define PORT 9090
+#define BUFFER_SIZE 1024
+
+
+int is_allowed(const char *role, const char *command);
+
+int main() {
+WSADATA wsa;
+SOCKET sockfd;
+struct sockaddr_in server_addr;
+char buffer[BUFFER_SIZE];
+char role[20];
+
+
+if (WSAStartup(MAKEWORD(2,2), &wsa) != 0) {
+printf("Gabim në Winsock\n");
+return 1;
+}
+
+
+sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+if (sockfd == INVALID_SOCKET) {
+printf("Gabim në socket\n");
+return 1;
+}
+
+server_addr.sin_family = AF_INET;
+server_addr.sin_port = htons(PORT);
+server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+
+printf("Zgjedh rolin (admin / read): ");
+scanf("%19s", role);
+getchar();
+
+printf("U lidhe si: %s\n", role);
+
+while (1) {
+printf("\nShkruaj komanden: ");
+fgets(buffer, BUFFER_SIZE, stdin);
+
+buffer[strcspn(buffer, "\n")] = 0;
+
+if (strcmp(buffer, "exit") == 0) {
+printf("Duke u mbyllur...\n");
+break;
+}
+
+if (!is_allowed(role, buffer)) {
+printf("Nuk ke privilegje!\n");
+continue;
+}
+
+sendto(sockfd, buffer, strlen(buffer), 0,
+(struct sockaddr*)&server_addr, sizeof(server_addr));
+
+int len = sizeof(server_addr);
+int n = recvfrom(sockfd, buffer, BUFFER_SIZE - 1, 0,
+(struct sockaddr*)&server_addr, &len);
+
+if (n == SOCKET_ERROR) {
+printf("Gabim në marrje\n");
+continue;
+}
+
+buffer[n] = '\0';
+printf("Serveri: %s\n", buffer);
+}
+
+closesocket(sockfd);
+WSACleanup();
+
+return 0;
+}
